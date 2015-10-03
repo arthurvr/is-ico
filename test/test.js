a74ee5a15699a893cb8a816a1db7d72825b1feef
Arthur Verschaeve <contact@arthurverschaeve.be>
2015-10-03 22:35:34 +0200

    ES2015ify tests

--- test/test.js
+++ test/test.js
@@ -1,16 +1,15 @@
-'use strict';
-var test = require('ava');
-var readChunk = require('read-chunk');
-var isIco = require('../');
+import test from 'ava';
+import readChunk from 'read-chunk';
+import isIco from '../';
 
 function check(filename) {
   return isIco(readChunk.sync(filename, 0, 4));
 }
 
-test('should return true when passing an ico', function (t) {
+test('should return true when passing an ico', t => {
   t.true(check('test/fixture.ico'));
 });
 
-test('should return false when not passing an ico', function (t) {
+test('should return false when not passing an ico', t => {
   t.false(check('test/fixture.avi'));
 });
